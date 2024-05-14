50303473a3b3b16a6abaf4b897a20f891b74e7d9
Momoiyk0301 <moise.iyakaremye@gmail.com>
2024-05-14 22:07:03 +0200

    Changement base de donnée
    OVH -> Clever Cloud

--- config.js
+++ config.js
@@ -2,11 +2,11 @@
 
 //Connexion vers la base de donnée
 const dbConfig = {
-    host: 'im112802-001.eu.clouddb.ovh.net',
-    port: '35349',
-    user: 'admin',
-    password: 'Momoiyk0301',
-    database: 'bar_wsf',
+    host: 'bjbfvx0iitnjtock1c3g-mysql.services.clever-cloud.com',
+    port: '3306',
+    user: 'uhhfyxwn68f7rgi7',
+    password: 'MlGLk1jUdOb7fyVUKpcE9',
+    database: 'bjbfvx0iitnjtock1c3g',
   };
   
   module.exports = dbConfig  
